f26fae108eb31d9a30d20b51b1915c3a503b877e
selfcg <selfcg@gmail.com>
2024-07-17 14:19:15 -0400

    added base html and folder structure

---
+++ src/utils/dom-utils.js
@@ -0,0 +1,14 @@
+class DOMHelper {
+  constructor() {
+    console.log('DOMHelper loaded');
+  }
+
+  /**
+   *
+   * @param {*} domNode
+   * @param {*} className
+   */
+  static render(domNode, className) {
+    console.log('Called DOMHelper.render');
+  }
+}
